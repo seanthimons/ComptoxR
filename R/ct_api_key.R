0ffc0587b241e2b297fff2b56c4329ec4c7c5d7b
Sean Thimons <thimons.sean@epa.gov>
2023-06-22 09:05:55 -0400

    Updated desc file.

--- R/ct_api_key.R
+++ R/ct_api_key.R
@@ -1,11 +1,11 @@
 #' Retrieves CCTE API key from the Sys Environment
-#' 
+#'
 #' An API Key is needed to access these APIs. Each user will need a specific key for each application. Please send an email to request an API key.
 #' Set API key in Sys Environment to the variable `ccte_api_key`
 #' to:`ccte_api@epa.gov`
 #' subject: API Key Request
-#' 
-#' 
+#'
+#'
 #' @export
 
 ct_api_key <- function() {
