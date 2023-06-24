21bfc6684a535b3985cfec62179a8aeecbcb18ac
seanthimons <thimons.sean@gmail.com>
2023-06-24 14:38:05 -0400

    Updated GenRA funcs, added data files for toxprint chemotypes

--- R/data.R
+++ R/data.R
@@ -13,3 +13,11 @@
 #' @format## `genra_engine`
 #' A tibble of possible choices for GenRA functions.
 'genra_engine'
+
+#' Table of ToxPrint Enrichment table values to build against.
+#'
+#' A tibble of possible Chemotypers Toxprints that have been enriched against in-vitro assay results
+#'
+#' @format `toxprint_dict`
+#'
+'toxprint_dict'
