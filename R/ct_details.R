7554f4d67fee002ae604dd7badeb287785e1c0bc
Sean Thimons <thimons.sean@epa.gov>
2023-06-28 18:00:53 -0400

    Updated scratch

--- R/ct_details.R
+++ R/ct_details.R
@@ -2,12 +2,13 @@
 #'
 #'
 #' @param dtxsid A single DTXSID (in quotes) or a list to be queried
+#' @param projection Subsets the returned dataframe based on desired provided results. Defaults to `chemicaldetailstandard`.
 #' @param server Defaults to public API, private requires USEPA VPN
 #' @param ccte_api_key Checks for API key in Sys env
 #' @return Returns a tibble with results
 #' @export
 
-ct_details <- function(query, server = 1, ccte_api_key = NULL){
+ct_details <- function(query, projection = c('chemicaldetailall','chemicaldetailstandard','chemicalidentifier', 'chemicalstructure','ntatoolkit'), server = 1, ccte_api_key = NULL){
 
   if (is.null(ccte_api_key)) {
     token <- ct_api_key()
@@ -27,7 +28,9 @@ ct_details <- function(query, server = 1, ccte_api_key = NULL){
   cat("Searching for compound details....\n")
   surl <- "chemical/detail/search/by-dtxsid/"
 
-  urls <- paste0(burl, surl, query)
+  if(identical(projection, c('chemicaldetailall','chemicaldetailstandard','chemicalidentifier', 'chemicalstructure','ntatoolkit'))){proj <- 'chemicaldetailstandard'}else{proj <- projection}
+
+  urls <- paste0(burl, surl, query,'?projection=',proj)
 
   df <- map_dfr(urls, ~{
 
