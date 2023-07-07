63cd8e3ea2cd76a0e2fbddbda21e8bdefc024866
seanthimons <thimons.sean@gmail.com>
2023-07-06 22:40:39 -0400

    Progress on hc_table and tp calculations

--- R/hc_meta.R
+++ R/hc_meta.R
@@ -2,45 +2,67 @@
 #'
 #' Will return in the console the endpoint coverage before weighing.
 #'
-#' @param hc_table Takes a the result of a `hc_table()` function and returns a table of scores (0-1) on the amount of columns by percent each compound has.
+#' @param table
+#' @param suffix
 #'
 #' @return A tibble of results
 #' @export
 
-hc_compound_coverage <- function(hc_table){
-  coverage_score <- hc_table %>%
-    select(c(!contains('_amount'))) %>%
-    mutate(data_coverage = (rowSums(is.na(.))))
+hc_compound_coverage <- function(table, suffix = NA){
+  if(is.na(suffix)){
+    cat('\nMissing search parameter!\n')
+    coverage_score <- table %>%
+      mutate(data_coverage = (rowSums(is.na(.))))
 
-  j = length(coverage_score)-1
 
-  coverage_score <- coverage_score %>%
-    mutate(data_coverage = 1-data_coverage/j) %>%
-    select(compound, data_coverage)
+    coverage_score <- coverage_score %>%
+      mutate(data_coverage = 1-(data_coverage/(ncol(coverage_score)-2))) %>%
+      select(compound, data_coverage)
 
-  return(coverage_score)
+    return(coverage_score)
+    }
+  else{
+    coverage_score <- table %>%
+      select(c(!contains(suffix))) %>%
+      mutate(data_coverage = (rowSums(is.na(.))))
+
+
+    coverage_score <- coverage_score %>%
+      mutate(data_coverage = 1-(data_coverage/(ncol(coverage_score)-2))) %>%
+      select(compound, data_coverage)
+
+  return(coverage_score)}
 }
 
 #' Per endpoint - compound availability scoring
 #'
-#' @param hc_table Takes a the result of a `hc_table()` function and returns a table of scores (0-1) on the amount of compounds by percent each endpoint has.
+#' @param table Takes a the result of a `hc_table()` function and returns a table of scores (0-1) on the amount of compounds by percent each endpoint has.
 #' @param filter A number(0-1) to cut off endpoint by for weighing. By default, sets to 0.5.
 #'
 #' @return A tibble of results
 #' @export
 
-hc_endpoint_coverage <- function(hc_table, filter = NA){
-  j = nrow(hc_table)
+hc_endpoint_coverage <- function(table, ID = NA, suffix = NA, filter = NA){
 
   if(is.na(filter) == T){filt_score = 0.5}else(filt_score <- filter)
-
-  endpoint_score <- hc_table %>%
-    select(!c(compound)) %>%
-    select(c(contains('_amount'))) %>%
+  if(is.na(suffix) == T){
+    endpoint_score <- table %>%
+    select(!c(ID)) %>%
     map( ~sum(is.na(.))) %>%
     as.data.frame() %>%
-    mutate(across(everything(), ~ 1-(.x/j))) %>%
+    mutate(across(everything(), ~ 1-(.x/nrow(table)))) %>%
     pivot_longer(everything(), names_to = 'endpoint', values_to = 'score')
+  }else{
+    endpoint_score <- table %>%
+    select(!c(ID)) %>%
+    select(c(contains(suffix))) %>%
+    map( ~sum(is.na(.))) %>%
+    as.data.frame() %>%
+    mutate(across(everything(), ~ 1-(.x/nrow(table)))) %>%
+    pivot_longer(everything(), names_to = 'endpoint', values_to = 'score')
+  }
+
+    #debug
     print(endpoint_score)
 
     endpoint_filt_weight <- endpoint_score %>%
@@ -48,4 +70,11 @@ hc_endpoint_coverage <- function(hc_table, filter = NA){
     mutate(weight = 1)
 
   return(endpoint_filt_weight)
+}
+
+hc_bias <- function(table, bias){
+
+
+
+
 }
