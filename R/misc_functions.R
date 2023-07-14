d76863ff891051179d608448b42ed826cf6117c2
Sean Thimons <thimons.sean@epa.gov>
2023-07-14 15:52:13 -0400

    Broke out the cancer, skin-eye, genotox functions. TP funcs are fixed.

--- R/misc_functions.R
+++ R/misc_functions.R
@@ -46,33 +46,3 @@ min2 <- function(x){
 geometric.mean <- function(x,na.rm=TRUE)  {
     exp(mean(log(x[x > 0]),na.rm=na.rm)) }
 
-
-#' Title
-#'
-#' @param x
-#'
-#' @return
-#' @export
-
-tp_single_score <- function(x){
-
-  (x - min(x, na.rm = TRUE))/diff(range(x, na.rm = TRUE))
-
-}
-
-#' Title
-#'
-#' @param hc_table
-#'
-#' @return
-#' @export
-
-tp_combined_score <- function(hc_table){
-
-  tp_scores <- hc_table %>% mutate(across(.cols = !contains('compound'), ~ tp_single_score(.x)))
-  tp_scores[is.na(tp_scores)] <- 0
-  tp_scores <- tp_scores %>%
-    rowwise() %>%
-    mutate(toxpi_score = sum(c_across(cols = !contains('compound'))))
-  return(tp_scores)
-}
