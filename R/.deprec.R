58f198c5ab87d0d58bf09a30bc1dd16fb9f4a652
seanthimons <thimons.sean@gmail.com>
2024-05-17 15:08:15 -0400

    Pre commit-updating ct_search to adjust for bad responses from POST requests. Will now just send GET requests for all three search types.

--- R/.deprec.R
+++ R/.deprec.R
@@ -1,12 +1,3 @@
-#' Search for compounds by mass range
-#'
-#' Search for any MS-ready compounds that are between the queried mass range. Also removes multicomponent compounds.
-#'
-#' @param start Starting mass range
-#' @param end Ending mass range
-#' @param ccte_api_key Checks for API key in Sys env
-#' @param debug Flag to show API calls
-#' @return Returns a tibble with results
 
 ct_search_mass <- function(start, end, ccte_api_key = NULL, debug = F){
 
@@ -37,17 +28,6 @@ ct_search_mass <- function(start, end, ccte_api_key = NULL, debug = F){
 
   return(df)
 }
-
-
-#' Search for compounds by formula
-#'
-#' Search for any MS-ready compounds by a generic chemical formula. Will not return any compound that is classified as multicomponent.
-#'
-#' @param query A string of a generic formula to search for
-#' @param ccte_api_key Checks for API key in Sys env
-#' @param debug Flag to show API calls
-#' @return Returns a tibble with results
-
 
 ct_search_formula <- function(query, ccte_api_key = NULL, debug = F){
 
@@ -129,5 +109,67 @@ ct_name <- function(query,
   df <-df %>% as_tibble()
   return(df)
 }
+
+#' Colorize text for display in the terminal.
+#'
+#' If R is not currently running in a system that supports terminal colors
+#' the text will be returned unchanged.
+#'
+#' Allowed colors are: black, blue, brown, cyan, dark gray, green, light
+#' blue, light cyan, light gray, light green, light purple, light red,
+#' purple, red, white, yellow
+#'
+#' Taken from `testthat` package `@0e70997`
+
+#'
+#' @param text character vector
+#' @param fg foreground color, defaults to white
+#' @param bg background color, defaults to transparent
+#' @export
+
+colorize <- function(text, fg = "white", bg = NULL) {
+
+  col <- .fg_colours[tolower(fg)]
+  #print(col)
+
+  if (!is.null(bg)) {
+    col <- paste0(col, ";",.bg_colours[tolower(bg)])
+    #print(col)
+  }
+
+  paste0("\033[",col,';m', text, "\033[0m")
+}
+
+.fg_colours <- c(
+  "black" = "0;30",
+  "blue" = "0;34",
+  "green" = "0;32",
+  "cyan" = "0;36",
+  "red" = "0;31",
+  "purple" = "0;35",
+  "brown" = "0;33",
+  "light gray" = "0;37",
+  "dark gray" = "1;30",
+  "light blue" = "1;34",
+  "light green" = "1;32",
+  "light cyan" = "1;36",
+  "light red" = "1;31",
+  "light purple" = "1;35",
+  "yellow" = "1;33",
+  "white" = "1;37"
+)
+
+.bg_colours <- c(
+  "black" = "40",
+  "red" = "41",
+  "green" = "42",
+  "brown" = "43",
+  "blue" = "44",
+  "purple" = "45",
+  "cyan" = "46",
+  "light gray" = "47"
+)
+
+
 
 
