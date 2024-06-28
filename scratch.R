22246d6eb2f24cadbf51bccca8419ffaa01d65ec
Sean Thimons <thimons.sean@epa.gov>
2024-06-28 15:46:28 -0400

    - Fixed chemi_search - Clean up of ct_descriptors

--- scratch.R
+++ scratch.R
@@ -1,97 +1,3 @@
-
-# testing -----------------------------------------------------------------
-
-s1 <- chemi_search(query = dtx_list$dtxsid[10],
-                   searchType = 'similar',
-                   min_similarity = 0.7
-                   )
-
-s2 <- chemi_hazard_dev(query = dtx_list$dtxsid[29],
-                   analogs = 'similar',
-                   min_sim = 0.8
-                   )
-
-query <- dtx_list$dtxsid[29]
-chemicals <- vector(mode = 'list', length = length(query))
-
-chemicals <- map2(chemicals,query,
-                  ~{.x <- list(chemical = list(
-                    sid = .y))}
-)
-
-#orig----
-payload <- jsonlite::fromJSON('{"chemicals":[{"chemical":{"id":"20182","cid":"DTXCID30182","sid":"DTXSID7020182","casrn":"80-05-7","name":"Bisphenol A","smiles":"C(C)(C)(C1C=CC(O)=CC=1)C1C=CC(O)=CC=1","canonicalSmiles":"CC(C)(C1C=CC(O)=CC=1)C1C=CC(O)=CC=1","inchi":"InChI=1S/C15H16O2/c1-15(2,11-3-7-13(16)8-4-11)12-5-9-14(17)10-6-12/h3-10,16-17H,1-2H3","inchiKey":"IISBACLAFKSPIT-UHFFFAOYSA-N","checked":true},"properties":{}}],"options":{"cts":null,"minSimilarity":"0.9","analogsSearchType":"SIMILAR"}}', simplifyVector = F)
-payload <- jsonlite::toJSON(payload, auto_unbox = T)
-
-payload$options$cts <- 'null'
-
-payload <- list(
-  chemicals = list(
-    list(
-      chemical = list(
-        #id = "20182"
-        #,cid = "DTXCID30182"
-        #sid = "DTXSID7020182"
-        #,casrn = "80-05-7"
-        #,name = "Bisphenol A"
-        smiles = "C(C)(C)(C1C=CC(O)=CC=1)C1C=CC(O)=CC=1"
-        #,canonicalSmiles = "CC(C)(C1C=CC(O)=CC=1)C1C=CC(O)=CC=1"
-        #,inchi = "InChI=1S/C15H16O2/c1-15(2,11-3-7-13(16)8-4-11)12-5-9-14(17)10-6-12/h3-10,16-17H,1-2H3"
-        #,inchiKey = "IISBACLAFKSPIT-UHFFFAOYSA-N"
-        ,checked = TRUE
-      ),
-      properties = NULL
-    )),
-  options = list(
-    cts = NA,
-    analogsSearchType = 'SIMILAR',
-    minSimilarity = 0.9
-  )
-)
-
-
-#mod-----
-chemical <- ct_details(query = query, projection = 'structure') %>%
-  select(dtxsid, smiles) %>%
-  rename(sid = dtxsid) %>%
-  as.list()
-
-payload <- list(
-  chemicals = list(
-    list(
-      chemical = chemical,
-      properties = NULL
-    )),
-  options = list(
-    cts = NA,
-    analogsSearchType = 'SIMILAR',
-    minSimilarity = 0.9
-  )
-)
-
-jsonlite::toJSON(payload, auto_unbox = T)
-
-#response----
-
-response <- httr::POST(
-  url = "https://hcd.rtpnc.epa.gov/api/hazard",
-  body = payload,
-  content_type("application/json"),
-  accept("application/json, text/plain, */*"),
-  encode = 'json',
-  progress() #progress bar
-)
-
-df <- content(response, "text", encoding = 'UTF-8') %>%
-  jsonlite::fromJSON(simplifyVector = FALSE)
-
-df <- s1
-
-df1 <- df %>%
-  pluck(., 'records') %>%
-  map(., as_tibble) %>%
-  list_rbind()
-
 groq <- function(query){
 
   GROQ_API_KEY <- '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'
@@ -125,118 +31,33 @@ groq <- function(query){
   return(res)
 }
 
-df <- ct_exposure(query = dtx_list[6:8], param = 'func_use')
-
-df$data$`functional-use/search` %>%
-  count(dtxsid, functioncategory) %>%
-  #filter(., dtxsid %in% dtx_list[6:8])
-  ggplot(.) +
-  aes(x = functioncategory,
-      y = n,
-    #  color = harmonizedFunctionalUse
-      fill = functioncategory) +
-  geom_bar(stat = 'identity') +
-  colorspace::scale_fill_discrete_qualitative() +
-  #coord_polar() +
-  facet_wrap(vars(dtxsid))
-
-
 #testing ----
 
 df <- chemi_search(
-  searchType = 'features',
-                          filter_results = F,
-                          #filter_inc = list('charged'),
-                          element_inc = "Pb",
-                          element_exc = "ALL",
-                          debug = F)
+  query = 'DTXSID9025114',
+  searchType = 'exact')
 
-df1 <-   ct_details(query = df$sid)
-
+df <- chemi_search(
+  query = 'DTXSID9025114',
+  searchType = 'substructure',
+  min_similarity = 0.8)
 
 df <- chemi_search(
   query = 'DTXSID9025114',
   searchType = 'similar',
-  min_similarity = 0.8,
-  min_toxicity = 'A')
+  min_similarity = 0.8)
 
 df <- chemi_search(
   searchType = 'features',
-  filter_results = T,
-  filter_inc = c('isotopes','charged'),
   element_inc = 'Cr',
-  element_exc = 'ALL',
-  min_toxicity = 'A')
+  element_exc = 'ALL')
 
+df <- ct_details(query = df$sid, projection = 'structure')
 
-df <- pt$oxidation_state %>%
-  #filter(element == 'arsenic') %>%
-  select(search)
-
-df <- ct_search(type = 'string', search_param = 'equal', query = df$search)
-df <- df %>%
-  filter(!is.na(dtxsid)) %>%
-  mutate(dim = nchar(smiles)) %>%
-  filter(dim <= 7)
-
-
-######
-query <- prodwater$dtxsid[1:10]
-
-burl <- Sys.getenv('burl')
-
-surl <- "chemical/synonym/search/by-dtxsid/"
-
-urls <- paste0(burl, surl, query)
-
-df <- map(urls, ~{
-
-  response <- VERB("GET", url = .x, add_headers("x-api-key" = token))
-  df <- fromJSON(content(response, as = "text", encoding = "UTF-8"))
-}, progress())
-
-names(df) <- query
-
-####
-
-headers <- c(
-  `x-api-key` = ct_api_key()
-)
-
-burl <- Sys.getenv('burl')
-
-#string_url <- 'chemical/search/equal/'
-string_url <- '/chemical/search/equal/'
-
-  query <- unique(pt$symbol)
-#
-#   sublists <- split(query, rep(1:ceiling(length(query)/200), each = 200, length.out = length(query)))
-#   sublists <- map(sublists, as.list)
-
-  df <- map(query, ~{
-
-    #.x <- paste0(.x)
-
-    .x <- GET(
-      url = paste0(burl, string_url, .x),
-      #body = .x,
-      add_headers(.headers = headers)
-      #content_type("application/json"),
-      #accept("application/json"),
-      #encode = "json",
-      #progress() # progress bar
-    )
-
-    .x <- content(.x, "text", encoding = "UTF-8") %>%
-      jsonlite::fromJSON(simplifyVector = TRUE) #%>%
-      # discard(., is_list) %>%
-      # distinct(.keep_all = T) %>%
-      # filter(!is.na(rank))
-
-  }, .progress = T) #%>% list_rbind()
-
-
-#####################
-
+list("Search type" = "{searchType}",
+     "Similarity type" = "{similarity_type}",
+     "Minimum simularity" = "{min_sim}",
+     "Minimum toxicity" = "{min_toxicity}")
+cli::cli_dl()
 
 
