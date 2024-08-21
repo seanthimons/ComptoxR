a8ea68f07c8f1e96d3398a777c4d2a0c204b9354
seanthimons <thimons.sean@epa.gov>
2024-08-21 18:09:06 -0400

    - Added webchem re-exports for CASRN checking and coercing

--- R/ComptoxR-package.R
+++ R/ComptoxR-package.R
@@ -2,24 +2,7 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @import cli
-#' @import dplyr
-#' @import httr
-#' @import purrr
-#' @import stringr
-#' @import tidyr
-#' @importFrom DT datatable
-#' @importFrom DT formatStyle
-#' @importFrom DT styleEqual
-#' @importFrom jsonlite fromJSON
-#' @importFrom magrittr %>%
-#' @importFrom pbapply pbapply
-#' @importFrom pbapply pblapply
-#' @importFrom pbapply pboptions
-#' @importFrom pillar glimpse
-#' @importFrom tibble as_tibble
 #' @importFrom webchem as.cas
 #' @importFrom webchem is.cas
-#' @importFrom webchem pc_sect
 ## usethis namespace: end
 NULL
