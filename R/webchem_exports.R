75b9f5b8026fd6569acf16d12227a6dadc490f6c
seanthimons <thimons.sean@gmail.com>
2024-08-21 18:09:06 -0400

    - Added webchem re-exports for CASRN checking and coercing

---
+++ R/webchem_exports.R
@@ -0,0 +1,15 @@
+
+#' @importFrom webchem as.cas
+#' @export
+
+webchem::as.cas
+
+#' @importFrom webchem is.cas
+#' @export
+
+webchem::is.cas
+
+#' @importFrom webchem pc_sect
+#' @export
+
+webchem::pc_sect
