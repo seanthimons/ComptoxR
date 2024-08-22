b91ae59a00a172ec1160850771c8c4b2fbf08f09
seanthimons <thimons.sean@epa.gov>
2024-08-22 18:11:03 -0400

    - Patched ct_search, now checks for mixed queries of CASRNs and names

--- scratch.R
+++ scratch.R
@@ -117,225 +117,24 @@ chemicals <- map(query, ~ {
     fromJSON(simplifyVector = TRUE)
 }
 
-# ct_search ---------------------------------------------------------------
 
-ct_search <- function(
-    type = c(
-      "string",
-      "mass"
-      # ,'formula'
-    ),
-    search_param = c(
-      "equal",
-      "start-with",
-      "substring"
-    ),
-    query,
-    suggestions = TRUE,
-    ccte_api_key = NULL) {
-  if (is.null(ccte_api_key)) {
-    token <- ct_api_key()
-  }
+cas_list <- rio::import('cas_list.csv') %>%
+  select(analyte, cas_number)
 
-  {
-    burl <- Sys.getenv("burl")
-    string_url <- "chemical/search/equal/"
-    formula_url <- "chemical/msready/search/by-formula/"
-    mass_url <- "chemical/msready/search/by-mass/"
-  }
+comp <- ct_search(type = 'string', search_param = 'equal', query = cas_list$analyte, suggestions = T) %>%
+  select(raw_search, dtxsid, preferredName) %>%
+  rename(dtxsid_name = dtxsid,
+         prefname_name = preferredName)
 
-  if (missing(type)) {
-    cli::cli_abort("Missing type search")
-  }
+cas <- ct_search(type = 'string', search_param = 'equal', query = cas_list$cas_number, suggestions = T) %>%
+  select(raw_search, dtxsid, preferredName) %>%
+  rename(dtxsid_cas = dtxsid,
+        prefname_cas = preferredName)
 
-  # Mass --------------------------------------------------------------------
+cas_cur <- left_join(cas_list, comp, join_by(analyte == raw_search)) %>%
+  left_join(., cas, join_by(cas_number == raw_search)) %>%
+  mutate(auth = if_else(dtxsid_cas == dtxsid_name, T, F))
 
 
-  if (type == "mass" & missing(search_param)) {
-    cli::cli_abort("Missing mass range!")
-  }
-
-  if (type == "mass" & !missing(search_param)) {
-    if (!is.numeric(query)) {
-      query <- as.numeric(query)
-    }
-
-    if (!is.numeric(search_param)) {
-      search_param <- as.numeric(search_param)
-    }
-
-    payload <- list(
-      masses = query,
-      error = search_param
-    )
-
-    cli::cli_rule(left = "Mass Payload options")
-    cli::cli_dl(c(
-      "Masses" = "{query}",
-      "Error" = "{search_param}"
-    ))
-
-    df <- POST(
-      url = paste0(burl, mass_url),
-      body = jsonlite::toJSON(payload, auto_unbox = T),
-      add_headers(`x-api-key` = token),
-      content_type("application/json"),
-      accept("application/json"),
-      encode = "json",
-      progress() # progress bar
-    )
-
-    df <- content(df, "text", encoding = "UTF-8") %>%
-      jsonlite::fromJSON(simplifyVector = FALSE)
-
-    return(df)
-  }
-
-  # String ------------------------------------------------------------------
-
-  if (missing(suggestions)) {
-    cli::cli_alert_warning("Defaulting to including suggestions!")
-    cli::cli_alert_warning("Did you forget to specify `suggestions`?")
-    cli::cli_text("")
-
-    suggestions <- TRUE
-  }
-
-  if (type == "string" & missing(search_param)) {
-    cli::cli_alert_warning("Defaulting to exact search!")
-    cli::cli_alert_warning("Did you forget to specify which `search_param`?")
-
-    # search_param <- match.arg(search_param, c('equal', 'start-with', 'substring'))
-
-    df <- .string_search(query, sp = "equal", sugs = suggestions)
-  } else {
-    search_param <- match.arg(search_param, c("equal", "start-with", "substring"))
-
-    df <- .string_search(query, sp = search_param, sugs = suggestions)
-  }
-
-  if (suggestions == FALSE) {
-    df <- df %>%
-      filter(!is.na(rank)) %>%
-      select(!(contains("suggestion_")))
-  } else {
-    return(df)
-  }
-}
-
-string_search <- function(query, sp = 'equal') {
-  headers <- c(
-    `x-api-key` = ct_api_key()
-  )
-
-  burl <- Sys.getenv("burl")
-
-  # cli::cli_rule(left = "String Payload options")
-  # cli::cli_dl(c(
-  #   "Compound count" = "{length(query)}",
-  #   "Search type" = "{sp}",
-  #   "Suggestions" = "{sugs}"
-  # ))
-
-  query_search <- query #%>% str_replace_all(., pattern = " ", replacement = "%20")
-
-  # Exact -------------------------------------------------------------------
-
-  if (sp == "equal") {
-    surl <- "chemical/search/"
-
-    urls <- do.call(paste0, expand.grid(burl, surl, sp, "/", query_search))
-
-    df <- map(urls, possibly(~ {
-      response <- VERB("GET", url = .x, add_headers("x-api-key" = ct_api_key()))
-      df <- fromJSON(content(response, as = "text", encoding = "UTF-8"))
-    }, otherwise = NULL), .progress = T) %>%
-      compact() %>%
-      set_names(., query) %>%
-      map_if(.,
-        .p = is.data.frame, ~ {
-          select(., -searchValue)
-        },
-        .else = ~ {
-          pluck(., "suggestions") %>%
-            as_tibble() %>%
-            mutate(idx = 1:n(), .before = value) %>%
-            pivot_wider(., names_prefix = "suggestion_", names_from = idx)
-        }
-      ) %>%
-      list_rbind(., names_to = "searchValue")
-
-    # string_url <- 'chemical/search/equal/'
-    #
-    # if(length(query) > 200){
-    #
-    #   cli::cli_alert_warning('Large request detected!')
-    #
-    #   sublists <- split(query, rep(1:ceiling(length(query)/200), each = 200, length.out = length(query)))
-    #   sublists <- map(sublists, as.list)
-    #
-    #   df <- map(sublists, ~{
-    #
-    #     .x <- paste0(.x)
-    #
-    #     .x <- POST(
-    #       url = paste0(burl, string_url),
-    #       body = .x,
-    #       add_headers(.headers = headers),
-    #       content_type("application/json"),
-    #       accept("application/json"),
-    #       encode = "json",
-    #       progress() # progress bar
-    #     )
-    #
-    #     .x <- content(.x, "text", encoding = "UTF-8") %>%
-    #       jsonlite::fromJSON(simplifyVector = TRUE)
-    #
-    #   }) %>% list_rbind()
-    #
-    # }else{
-    #
-    #   response <- POST(
-    #     url = paste0(burl, string_url),
-    #     body = query,
-    #     content_type("application/json"),
-    #     accept("application/json"),
-    #     encode = 'json',
-    #     add_headers(`x-api-key` = ct_api_key()),
-    #     progress() #progress bar
-    #   )
-    #
-    #   df <- content(response, "text", encoding = 'UTF-8') %>%
-    #     jsonlite::fromJSON(simplifyVector = TRUE)
-  } else {
-    # Substring ---------------------------------------------------------------
 
 
-    if (sp %in% c("start-with", "substring")) {
-      surl <- "chemical/search/"
-
-      urls <- do.call(paste0, expand.grid(burl, surl, search_param, "/", query_search))
-
-      df <- map(urls, possibly(~ {
-        response <- VERB("GET", url = .x, add_headers("x-api-key" = ct_api_key()))
-        df <- fromJSON(content(response, as = "text", encoding = "UTF-8"))
-      }, otherwise = NULL), .progress = T) %>%
-        compact() %>%
-        set_names(., query) %>%
-        map_if(.,
-          .p = is.data.frame, ~ {
-            select(., -searchValue)
-          },
-          .else = ~ {
-            pluck(., "suggestions") %>%
-              as_tibble() %>%
-              mutate(idx = 1:n(), .before = value) %>%
-              pivot_wider(., names_prefix = "suggestion_", names_from = idx)
-          }
-        ) %>%
-        list_rbind(., names_to = "searchValue")
-    } else {
-      cli::cli_abort("Search parameter for `string` search failed!")
-    }
-  }
-}
