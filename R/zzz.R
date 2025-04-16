3754247b6dffeafc40a0e9e09c5a55896094a85c
seanthimons <141747936+seanthimons@users.noreply.github.com>
2025-04-16 18:25:41 -0400

    Update zzz.R
    -- Added new logic for servers and API endpoints

--- R/zzz.R
+++ R/zzz.R
@@ -20,7 +20,10 @@ run_setup <- function(){
       'https://api-ccte.epa.gov/chemical/health',
 
       'https://hcd.rtpnc.epa.gov/#/', #prod
-      'https://hazard-dev.sciencedataexperts.com/#/' #dev
+      'https://hazard-dev.sciencedataexperts.com/#/', #dev
+
+      'https://episuite.dev/EpiWebSuite/#/',
+      "https://cfpub.epa.gov/ecotox/index.cfm"
 
     )
 
@@ -58,6 +61,9 @@ run_setup <- function(){
   if(Sys.getenv('burl') == "" | Sys.getenv("chemi_burl") == ""){
     ct_server(server = 1)
     chemi_server(server = 1)
+    epi_server(server = 1)
+    eco_server(server = 1)
+
   }
 
   packageStartupMessage(
@@ -85,10 +91,149 @@ run_setup <- function(){
     cli::cli_alert_warning('Available API endpoints:')
     cli::cli_dl(c(
       'CompTox Chemistry Dashboard' = '{Sys.getenv("burl")}',
-      'Cheminformatics' =  '{Sys.getenv("chemi_burl")}'
+      'Cheminformatics' =  '{Sys.getenv("chemi_burl")}',
+      'ECOTOX' = '{Sys.getenv("eco_burl")}',
+      'EPI Suite' = '{Sys.getenv("epi_burl")}'
     ))
   })
 
   run_setup()
+
+}
+
+#' Set API endpoints for Comptox API endpoints
+#'
+#' @param server Defines what server to target
+#'
+#' @return Should return the Sys Env variable 'burl'
+#' @export
+
+ct_server <- function(server = NULL){
+  if (is.null(server)) {
+    {
+      cli::cli_alert_danger("Server URL reset!")
+      Sys.setenv("burl" = "")
+    }
+
+  } else {
+
+    switch(
+      as.character(server),
+      "1" = Sys.setenv('burl' = 'https://api-ccte.epa.gov/'),
+      "2" = Sys.setenv('burl' = 'https://api-ccte-stg.epa.gov/'),
+      {
+        cli::cli_alert_warning("\nServer URL reset!\n")
+        Sys.setenv("burl" = "")
+      }
+    )
+
+    Sys.getenv('burl')
+  }
+}
+
+#' Set API endpoints for Cheminformatics API endpoints
+#'
+#' @param server Defines what server to target
+#'
+#' @return Should return the Sys Env variable `chemi_burl`
+#' @export
+
+chemi_server <- function(server = NULL){
+
+  if (is.null(server)) {
+
+    {
+      cli::cli_alert_danger("Server URL reset!")
+      Sys.setenv("chemi_burl" = "")
+    }
+
+  } else {
+
+    switch(
+      as.character(server),
+      "1" = Sys.setenv("chemi_burl" = "https://hcd.rtpnc.epa.gov/"),
+      "2" = Sys.setenv("chemi_burl" = "https://hazard-dev.sciencedataexperts.com/"),
+      "3" = Sys.setenv("chemi_burl" = "https://ccte-cced-cheminformatics.epa.gov/"),
+      {
+        cli::cli_alert_warning("\nServer URL reset!\n")
+        Sys.setenv("chemi_burl" = "")
+      }
+    )
+
+    Sys.getenv("chemi_burl")
+  }
+}
+
+#' Set API endpoints for EPI Suite API endpoints
+#'
+#' @param server Defines what server to target
+#'
+#' @return Should return the Sys Env variable `epi_burl`
+#' @export
+
+epi_server <- function(server = NULL){
+
+  if (is.null(server)) {
+
+    {
+      cli::cli_alert_danger("Server URL reset!")
+      Sys.setenv("epi_burl" = "")
+    }
+
+  } else {
+
+    switch(
+      as.character(server),
+      "1" = Sys.setenv("epi_burl" = "https://episuite.dev/EpiWebSuite/api"),
+      {
+        cli::cli_alert_warning("\nServer URL reset!\n")
+        Sys.setenv("epi_burl" = "")
+      }
+    )
+
+    Sys.getenv("epi_burl")
+  }
+}
+
+#' Set API endpoints for ECOTOX API endpoints
+#'
+#' @param server Defines what server to target
+#'
+#' @return Should return the Sys Env variable `eco_burl`
+#' @export
+
+eco_server <- function(server = NULL){
+
+  if (is.null(server)) {
+
+    {
+      cli::cli_alert_danger("Server URL reset!")
+      Sys.setenv("eco_burl" = "")
+    }
+
+  } else {
+
+    switch(
+      as.character(server),
+      "2" = Sys.setenv("eco_burl" = "https://hcd.rtpnc.epa.gov/"),
+      "1" = Sys.setenv("eco_burl" = "http://127.0.0.1:5555"),
+      {
+        cli::cli_alert_warning("\nServer URL reset!\n")
+        Sys.setenv("eco_burl" = "")
+      }
+    )
+
+    Sys.getenv("eco_burl")
+  }
+}
+
+#' Reset all servers
+
+reset_servers <- function(){
+
+  ct_server()
+  chemi_server()
+  epi_server()
+  eco_server()
 
 }
