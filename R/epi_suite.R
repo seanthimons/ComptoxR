fa85dc5886f4d386cd24e1a472c71d5ab1810df1
seanthimons <thimons.sean@gmail.com>
2025-05-19 16:15:18 -0400

    Update epi_suite.R

--- R/epi_suite.R
+++ R/epi_suite.R
@@ -57,11 +57,7 @@ epi_suite_analysis <- function(query){
 
   #Chose to have the data be pulled as a tibble rather than a list for diagnositic purposes
 
-  query_list <- epi_suite_search(query_list)
-
-  query_dict <- query_list
-
-  query_list <- query_list %>%
+  query_list <- epi_suite_search(query_list) %>%
     map(., ~pull(.x, 'cas'))
 
   req_list <- map(query_list, ~{
@@ -69,8 +65,6 @@ epi_suite_analysis <- function(query){
       req_url_path_append("/submit") %>%
       req_url_query(cas =  .x)
   })
-
-  cli::cli_alert_warning('Sending requests for analysis...')
 
   resps <- req_list %>%
     req_perform_sequential(., on_error = 'continue', progress = TRUE)
@@ -80,28 +74,15 @@ epi_suite_analysis <- function(query){
     resps_successes() %>%
     map(., ~resp_body_json(.x))
 
-  {
-    cli::cli_rule(left = 'EPI Suite analysis results')
-    cli::cli_dl()
-    cli::cli_li(
-      c('Compounds requested' = "{length(query_list)}"))
-    cli::cli_li(
-      c('Compounds found' = "{length(df)}"))
-    cli::cli_end()
-    cli::cat_line()
-    }
-
-# df <- list(dict = query_dict, data = df)
-
   return(df)
 }
 
-#' Pull data from EPI Suite query object
+#' Title
 #'
 #' @param response
 #' @param endpoints
 #'
-#' @returns list or dataframe
+#' @returns
 #' @export
 
 epi_suite_pull_data <- function(epi_obj, endpoints = NULL){
@@ -131,135 +112,48 @@ epi_suite_pull_data <- function(epi_obj, endpoints = NULL){
         list_rbind(names_to = 'raw_search')
     },
     'fate' = {
-      # biodegradationRate
-      biodegrade <- epi_obj %>%
+      epi_obj %>%
         map(., ~{
-          keep(., names(.x) %in% c("biodegradationRate")) %>%
-          pluck(., "biodegradationRate", "models") %>%
-          map(., ~discard_at(.x, 'factors')) %>%
-          map(., as_tibble) %>%
-          list_rbind() %>%
-          add_row(name = 'Readily Biodegradable?') %>%
-          mutate(
-            result = case_when(
-              str_detect(name, pattern = 'MITI Linear Model Prediction|MITI Non-Linear Model Prediction') & value >= 0.5 ~ 'degradable',
-              str_detect(name, pattern = 'MITI Linear Model Prediction|MITI Non-Linear Model Prediction') & value < 0.5 ~ 'not degradable',
-              str_detect(name, pattern = 'Linear Model Prediction|Non-Linear Model Prediction') & value >= 0.5 ~ 'fast',
-              str_detect(name, pattern = 'Linear Model Prediction|Non-Linear Model Prediction') & value < 0.5 ~ 'not fast',
-              str_detect(name, pattern = 'Ultimate Biodegradation Timeframe|Primary Biodegradation Timeframe') & between(value, 4, 5)  ~ 'hour-days',
-              str_detect(name, pattern = 'Ultimate Biodegradation Timeframe|Primary Biodegradation Timeframe') & between(value, 3, 4)  ~ 'days-weeks',
-              str_detect(name, pattern = 'Ultimate Biodegradation Timeframe|Primary Biodegradation Timeframe') & between(value, 2, 3)  ~ 'weeks-months',
-              str_detect(name, pattern = 'Ultimate Biodegradation Timeframe|Primary Biodegradation Timeframe') & between(value, 1, 2)  ~ 'months-longer',
-              str_detect(name, pattern = 'Ultimate Biodegradation Timeframe|Primary Biodegradation Timeframe') & value < 1  ~ 'longer',
-              str_detect(name, pattern = 'Ultimate Biodegradation Timeframe|Primary Biodegradation Timeframe') & value > 5  ~ 'hours',
-              str_detect(name, pattern = 'Anaerobic Model Prediction') & value >= 0.5 ~ 'fast',
-              str_detect(name, pattern = 'Anaerobic Model Prediction') & value < 0.5 ~ 'not fast',
-              .default = NA),
-            final = case_when(
-              name == "Ultimate Biodegradation Timeframe" & value >= 3 ~ TRUE,
-              name == "Ultimate Biodegradation Timeframe" & value < 3 ~ FALSE,
-              name == "MITI Linear Model Prediction" & value < 0.5 ~ FALSE,
-              name == "MITI Linear Model Prediction" & value >= 0.5 ~ TRUE,
-              .default = NA),
-            result = case_when(
-              name == 'Readily Biodegradable?' & sum(final == TRUE, na.rm = TRUE) == 2 ~ 'yes',
-              name == 'Readily Biodegradable?' & sum(final == TRUE, na.rm = TRUE) != 2 ~ 'no',
-              .default = result)
-            ) %>%
-            select(-final)
-        }) %>%
-        list_rbind(names_to = 'raw_search')
-
-      hydrocarbon_biodegrade <- epi_obj %>%
-        map(., ~{
-            keep(., names(.x) %in% c("hydrocarbonBiodegradationRate")) %>%
-            pluck(., "hydrocarbonBiodegradationRate", 'selectedValue') #%>% flatten()
-        }) %>%
-        map(., ~modify_at(., 'value', ~if(is.null(.x)) NA else .x)) %>%
-        map(., as_tibble) %>%
-        list_rbind(names_to = 'raw_search') %>%
-        filter(!is.na(value))
-
-      bioconc <- epi_obj %>%
-        map(., ~{
-          keep(., names(.x) %in% c("bioconcentration")) %>%
-          pluck(., 'bioconcentration')
-        }) %>%
-        map(.,
-            ~keep(., names(.x) %in% c('logBioconcentrationFactor', 'logBioaccumulationFactor')) %>%
-              as_tibble()
-          ) %>%
-        list_rbind(names_to = 'raw_search')
-
-      list(
-        bioconc = bioconc,
-        biodegrade = biodegrade,
-        hydrocarbon_biodegrade = hydrocarbon_biodegrade
-        )
+          keep(., names(.x) %in% c(""))
+        })
     },
     'transport' = {
       epi_obj %>%
         map(., ~{
-          keep(., names(.x) %in% c("fugacityModel")) %>%
-          map(., ~{
-            pluck(., 'model')
-            }) %>%
-          map(., ~keep(., names(.x) %in% c(
-            "Air",
-            "Water",
-            "Soil",
-            "Sediment",
-            "Persistence"))) %>%
-          flatten() %>%
-          map_at(., .at = c(
-            "Air",
-            "Water",
-            "Soil",
-            "Sediment"), .f = ~{pluck(., 1, 'MassAmount')}) %>%
-          map(., ~enframe(.x, name = 'name', value = 'value')) %>%
-          list_rbind(names_to = 'parameter') %>%
-          select(-name)
+          keep(., names(.x) %in% c(""))
         })},
     'treatment' = {
       epi_obj %>%
         map(., ~{
-          keep(., names(.x) %in% c("sewageTreatmentModel")) %>%
-          pluck(., 'sewageTreatmentModel', 'model') %>%
-          keep(., names(.) %in% c(
-            'TotalAir',
-            'TotalSludge',
-            'TotalBiodeg',
-            'FinalEffluent',
-            'TotalRemoval'
-          )) %>%
-          map(., ~pluck(., 'Percent')) %>%
-          flatten() %>%
-          as_tibble()
-        }) %>%
-        list_rbind(names_to = 'raw_search')
+          keep(., names(.x) %in% c(""))
+        })
     },
     'analogs' = {
 
       epi_obj %>%
         map(., ~{
-          keep(., names(.x) %in% c("analogs")) %>% flatten() %>% unname()
-        }) %>%
-        compact()
+          keep(., names(.x) %in% c("analogs"))
+        })
     },
     'water' = {
 
       epi_obj %>%
         map(., ~{
-          keep(., names(.x) %in% c("waterVolatilization")) %>%
-            pluck(., 1) %>%
-            discard_at(., 'parameters') %>%
-            as_tibble()
-        }) %>%
-        list_rbind(names_to = 'raw_search')
+          keep(., names(.x) %in% c(""))
+        })
     },
-    cli::cli_abort('Incorrect endpoint selection')
+    'air' = {
+
+      epi_obj %>%
+        map(., ~{
+          keep(., names(.x) %in% c(""))
+        })
+    }
+
   )
 
   return(df)
 }
 
+q3 <- epi_suite_pull_data(epi_obj = q2, endpoints = 'eco')
+q3 <- epi_suite_pull_data(epi_obj = q2, endpoints = 'analogs')
