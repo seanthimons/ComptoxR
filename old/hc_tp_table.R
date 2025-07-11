8746b4efaa9ffca3292c53ee07ff65eab2a59685
seanthimons <141747936+seanthimons@users.noreply.github.com>
2025-07-11 14:59:09 -0400

    Comment out deprecated functions in old R scripts
    All function definitions and related code in .deprec.R, chemi_table.R, hc_table.R, hc_tp_table.R, and toxpi.R have been commented out. This change disables execution of deprecated code while preserving the original logic for reference.

--- old/hc_tp_table.R
+++ old/hc_tp_table.R
@@ -1,87 +1,87 @@
-hc_tp_table <- function(hcd_tbl, filter_score = NA) {
-  #data coverage score
-  {
-    coverage_score <- hcd_tbl %>%
-      select(c(!contains('_amount'))) %>%
-      mutate(data_coverage = (rowSums(is.na(.))))
+# hc_tp_table <- function(hcd_tbl, filter_score = NA) {
+#   #data coverage score
+#   {
+#     coverage_score <- hcd_tbl %>%
+#       select(c(!contains('_amount'))) %>%
+#       mutate(data_coverage = (rowSums(is.na(.))))
 
-    j = length(coverage_score) - 4
+#     j = length(coverage_score) - 4
 
-    coverage_score <- coverage_score %>%
-      mutate(data_coverage = 1 - data_coverage / j) %>%
-      select(Compound, data_coverage)
-    #print(coverage_score)
-  }
+#     coverage_score <- coverage_score %>%
+#       mutate(data_coverage = 1 - data_coverage / j) %>%
+#       select(Compound, data_coverage)
+#     #print(coverage_score)
+#   }
 
-  #Per endpoint score
-  {
-    j = nrow(hcd_tbl)
+#   #Per endpoint score
+#   {
+#     j = nrow(hcd_tbl)
 
-    endpoint_score <- hcd_tbl %>%
-      select(!c(Compound:preferredName)) %>%
-      select(c(contains('_amount'))) %>%
-      map(~ sum(is.na(.))) %>%
-      as.data.frame() %>%
-      mutate(across(everything(), ~ 1 - (.x / j))) %>%
-      pivot_longer(everything(), names_to = 'endpoint', values_to = 'score') %>%
-      arrange(desc(score))
-    cat('\nSorted endpoint scores\n')
-    print(endpoint_score)
+#     endpoint_score <- hcd_tbl %>%
+#       select(!c(Compound:preferredName)) %>%
+#       select(c(contains('_amount'))) %>%
+#       map(~ sum(is.na(.))) %>%
+#       as.data.frame() %>%
+#       mutate(across(everything(), ~ 1 - (.x / j))) %>%
+#       pivot_longer(everything(), names_to = 'endpoint', values_to = 'score') %>%
+#       arrange(desc(score))
+#     cat('\nSorted endpoint scores\n')
+#     print(endpoint_score)
 
-    if (is.na(filter_score) == T) {
-      #filter_score <- 0
-      cat('\nNo filter for score applied!\n')
-      filter_score <- readline(prompt = 'Apply filter score [0-1]:\n')
-    }
+#     if (is.na(filter_score) == T) {
+#       #filter_score <- 0
+#       cat('\nNo filter for score applied!\n')
+#       filter_score <- readline(prompt = 'Apply filter score [0-1]:\n')
+#     }
 
-    filt_score = filter_score
+#     filt_score = filter_score
 
-    endpoint_filt_weight <- endpoint_score %>%
-      filter(score >= filt_score) %>% #%>% mutate(weight = 1)
-      as_tibble() %>%
-      arrange(desc(score))
-    cat('\nFiltered endpoint scores\n')
-    print(endpoint_filt_weight)
-    endpoint_filt_weight <- endpoint_filt_weight %>%
-      add_row(endpoint = 'Compound')
-  }
+#     endpoint_filt_weight <- endpoint_score %>%
+#       filter(score >= filt_score) %>% #%>% mutate(weight = 1)
+#       as_tibble() %>%
+#       arrange(desc(score))
+#     cat('\nFiltered endpoint scores\n')
+#     print(endpoint_filt_weight)
+#     endpoint_filt_weight <- endpoint_filt_weight %>%
+#       add_row(endpoint = 'Compound')
+#   }
 
-  tp <- hcd_tbl %>%
-    select(c('Compound', contains('_amount')))
+#   tp <- hcd_tbl %>%
+#     select(c('Compound', contains('_amount')))
 
-  tp <- tp[, which((names(tp) %in% endpoint_filt_weight$endpoint) == TRUE)]
+#   tp <- tp[, which((names(tp) %in% endpoint_filt_weight$endpoint) == TRUE)]
 
-  tp_scores <- tp %>%
-    mutate(across(
-      where(is.numeric),
-      ~ if (sd(na.omit(.)) == 0) {
-        ifelse(is.na(.), NA, 1)
-      } else {
-        tp_single_score(.) %>%
-          round(digits = 3)
-      }
-    ))
+#   tp_scores <- tp %>%
+#     mutate(across(
+#       where(is.numeric),
+#       ~ if (sd(na.omit(.)) == 0) {
+#         ifelse(is.na(.), NA, 1)
+#       } else {
+#         tp_single_score(.) %>%
+#           round(digits = 3)
+#       }
+#     ))
 
-  tp_scores2 <- tp_scores %>%
-    mutate(across(
-      where(is.numeric),
-      ~ {
-        min_val <- min(.[-which(. == 0)], na.rm = T)
-        replace(., . == 0, 0.5 * min_val)
-      }
-    ))
+#   tp_scores2 <- tp_scores %>%
+#     mutate(across(
+#       where(is.numeric),
+#       ~ {
+#         min_val <- min(.[-which(. == 0)], na.rm = T)
+#         replace(., . == 0, 0.5 * min_val)
+#       }
+#     ))
 
-  tp_scores2[is.na(tp_scores2)] <- 0
+#   tp_scores2[is.na(tp_scores2)] <- 0
 
-  tp_scores3 <- tp_scores2 %>%
-    rowwise() %>%
-    mutate(toxpi_score = sum(c_across(cols = !contains('compound'))))
+#   tp_scores3 <- tp_scores2 %>%
+#     rowwise() %>%
+#     mutate(toxpi_score = sum(c_across(cols = !contains('compound'))))
 
-  tp_table <- left_join(hcd_tbl, select(tp_scores3, Compound, toxpi_score)) %>%
-    arrange(desc(toxpi_score)) %>%
-    select(Compound:preferredName, toxpi_score)
+#   tp_table <- left_join(hcd_tbl, select(tp_scores3, Compound, toxpi_score)) %>%
+#     arrange(desc(toxpi_score)) %>%
+#     select(Compound:preferredName, toxpi_score)
 
-  tp_table <- left_join(tp_table, coverage_score)
+#   tp_table <- left_join(tp_table, coverage_score)
 
-  return(tp_table)
-}
+#   return(tp_table)
+# }
