ed1b5c3e29684672b0ac7bf33ca96e6cf9b8f669
seanthimons <141747936+seanthimons@users.noreply.github.com>
2025-08-29 15:32:17 -0400

    feat: add batch processing for large chemical queries

--- R/chemi_resolver.R
+++ R/chemi_resolver.R
@@ -20,101 +20,179 @@
 #' @return A list of resolved chemical information. Returns an empty list if no
 #'   results are found. Errors if the API request fails.
 #'
-#' @export List of lists
-chemi_resolver <- function(query, id_type = NULL, is_fuzzy = FALSE, fuzzy_type, mol = FALSE) {
-
+#' @export
+chemi_resolver <- function(
+	query,
+	id_type = NULL,
+	is_fuzzy = FALSE,
+	fuzzy_type,
+	mol = FALSE
+) {
 	# NOTE creates simple list if the length is 1, otherwise allows for boxed list
 
-	# TODO Prep for batched queries
+	init_query <- length(query)
 
-	if(length(query) == 1){
-		query <- list(query)
+	if (length(query) > 200) {
+		mult_request <- TRUE
+		mult_count = ceiling(length(query) / 200)
+		query <- split(
+			query,
+			rep(
+				1:ceiling(length(query) / 200),
+				each = 200,
+				length.out = length(query)
+			)
+		)
+	} else {
+		mult_request <- FALSE
+		mult_count = 1L
+		if (length(query) == 1) {
+			query <- list(query)
+		} else {
+			as.list(query)
+		}
 	}
 
 	# Check if the id_type argument is provided.
 	# If not, stop execution and inform the user.
-	if(missing(id_type) || is.null(id_type)){
-		cli::cli_alert_danger('Missing ID type, defaulting to {cli::col_red("AnyID")}')
+	if (missing(id_type) || is.null(id_type)) {
+		cli::cli_alert_danger(
+			'Missing ID type, defaulting to {cli::col_red("AnyID")}'
+		)
 		id_type <- 'AnyId'
 	}
 
 	# Validate the provided id_type against a predefined list of allowed values.
 	# match.arg will throw an error if the user's input is not one of the choices.
-	id_type <- rlang::arg_match(id_type, values = c('DTXSID', 'DTXCID', 'SMILES', 'MOL', 'CAS', 'Name', 'InChI', 'InChIKey', 'InChIKey_1', 'AnyId'))
+	id_type <- rlang::arg_match(
+		id_type,
+		values = c(
+			'DTXSID',
+			'DTXCID',
+			'SMILES',
+			'MOL',
+			'CAS',
+			'Name',
+			'InChI',
+			'InChIKey',
+			'InChIKey_1',
+			'AnyId'
+		)
+	)
 
 	# If fuzzy search is disabled, set the type to "Not". This ensures a
 	# valid default is passed to arg_match, as fuzzy_type lacks a default.
 	if (!is_fuzzy) {
 		fuzzy_type <- "Not"
-	# If fuzzy search is enabled, a `fuzzy_type` becomes mandatory.
+		# If fuzzy search is enabled, a `fuzzy_type` becomes mandatory.
 	} else if (missing(fuzzy_type) || is.null(fuzzy_type)) {
 		cli::cli_abort("When `is_fuzzy` is TRUE, a `fuzzy_type` must be provided.")
 	}
 	# Validate the provided fuzzy_type against a predefined list of allowed values.
 	# match.arg will throw an error if the user's input is not one of the choices.
-	fuzzy_type <- rlang::arg_match(fuzzy_type, values = c("Not", "Anywhere", "Start", "Word", "CloseSyntactic", "CloseSemantic"))
-
+	fuzzy_type <- rlang::arg_match(
+		fuzzy_type,
+		values = c(
+			"Not",
+			"Anywhere",
+			"Start",
+			"Word",
+			"CloseSyntactic",
+			"CloseSemantic"
+		)
+	)
 
 	# Returns the V3000 mol array
-	# Checks for parameter validity 
-	if(!is.logical(mol) || missing(mol) || is.null(mol)){
-		if(as.logical(Sys.getenv('run_verbose'))){
-			cli::cli_alert_warning('Mol parameter requires BOOLEAN value! Defaulting to {cli::col_red("FALSE")}.')
+	# Checks for parameter validity
+	if (!is.logical(mol) || missing(mol) || is.null(mol)) {
+		if (as.logical(Sys.getenv('run_verbose'))) {
+			cli::cli_alert_warning(
+				'Mol parameter requires BOOLEAN value! Defaulting to {cli::col_red("FALSE")}.'
+			)
 		}
 		mol <- FALSE
-	}else{
+	} else {
 		mol <- as.logical(mol)
 	}
-	
 
-  req <- request(Sys.getenv('chemi_burl')) %>%
-    req_method("POST") %>%
-    req_url_path_append("resolver/lookup") %>%
-    req_headers(Accept = "application/json, text/plain, */*") %>%
-    req_body_json(
-      list(
-        fuzzy = fuzzy_type,
-        ids = query,
-        idsType = id_type,
-        mol = mol
-      ),
-      auto_unbox = TRUE
-    )
-
-	if(as.logical(Sys.getenv('run_debug'))){
-
-		return(req %>% req_dry_run())
-		
+	if (mult_request) {
+		req_list <- map(
+			query,
+			function(query_part) {
+				req <- request(Sys.getenv('chemi_burl')) %>%
+					req_method("POST") %>%
+					req_url_path_append("resolver/lookup") %>%
+					req_headers(Accept = "application/json, text/plain, */*") %>%
+					req_body_json(
+						list(
+							fuzzy = fuzzy_type,
+							ids = query_part,
+							idsType = id_type,
+							mol = mol
+						),
+						auto_unbox = TRUE
+					)
+			}
+		)
+	} else {
+		req_list <- request(Sys.getenv('chemi_burl')) %>%
+			req_method("POST") %>%
+			req_url_path_append("resolver/lookup") %>%
+			req_headers(Accept = "application/json, text/plain, */*") %>%
+			req_body_json(
+				list(
+					fuzzy = fuzzy_type,
+					ids = query,
+					idsType = id_type,
+					mol = mol
+				),
+				auto_unbox = TRUE
+			)
 	}
-	
-  resp <- req %>%
-    req_perform()
 
-  if (resp_status(resp) < 200 || resp_status(resp) >= 300) {
-    cli::cli_abort(paste("API request failed with status", resp_status(resp)))
-  }
+	if (as.logical(Sys.getenv('run_debug'))) {
+		return(req_list %>% pluck(.,1) %>% req_dry_run())
+	}
 
-  body <- resp_body_json(resp)
+	if(mult_request){
+		resp_list <- req_list %>%
+		req_perform_sequential(., on_error = 'continue')
+	}else{
+		resp_list <- req_list %>%
+		req_perform()
+	}
 
-  if (length(body) == 0) {
-    cli::cli_alert_warning("No results found for the given query.")
-    return(list())
-  }
+	body_list <- resp_list %>%
+		map(., function(r) {
+			if (resp_status(r) < 200 || resp_status(r) >= 300) {
+				cli::cli_abort(paste(
+					"API request failed with status",
+					resp_status(r)
+				))
+			}
 
-  cli_rule(left = 'Resolver results')
-  cli_dl(
-    c(
-      'Number of compounds requested' = '{length(query)}',
-      'Number of compounds found' = '{length(body)}',
+			body <- resp_body_json(r)
+
+			if (length(body) == 0) {
+				cli::cli_alert_warning("No results found for the given query.")
+				return(list())
+			}
+		}) %>% list_c()
+
+	cli_rule(left = 'Resolver results')
+	cli_dl(
+		c(
+			'Number of compounds requested' = '{length(init_query)}',
+			'Number of batches' = '{mult_count}',
+			'Number of compounds found' = '{length(body_list)}',
 			'ID Type' = '{id_type}',
 			'Fuzzy Search' = '{is_fuzzy}',
 			'Fuzzy Type' = '{fuzzy_type}',
 			'Mol' = '{mol}'
-    )
-  )
-  cli::cli_rule()
-  cli::cli_end()
+		)
+	)
+	cli::cli_rule()
+	cli::cli_end()
 
-  map(body, ~ pluck(.x, 'chemical'))
+	# map(body, ~ pluck(.x, 'chemical'))
 }
-
