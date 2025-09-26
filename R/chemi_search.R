a51b4cc736b33b6cf423d4e4755095d1b62c6a19
seanthimons <141747936+seanthimons@users.noreply.github.com>
2025-09-26 17:51:01 -0400

    Refactor chemi_search and add mappings utility
    Refactored the chemi_search function for improved readability, parameter validation, and verbose/debug output control. Added R/mappings.R to centralize search parameter mappings, reducing duplication and potential errors in value translation.

--- R/chemi_search.R
+++ R/chemi_search.R
@@ -24,341 +24,342 @@
 #' @export
 
 chemi_search <- function(
-  query,
-  searchType = c(
-    "exact",
-    "substruture",
-    "similar",
-    "mass",
-    "hazard",
-    "features"
-  ),
-  similarity_type = c("tanimoto", "euclid", "tversky"),
-  min_similarity = NULL,
-  min_toxicity = c("VH", "H", "M", "L", "A"),
-  min_auth = c("auth", "screen", "qsar"),
-  hazard_name = c(
-    "acute_oral",
-    "acute_inhal",
-    "acute_dermal",
-    "cancer",
-    "geno",
-    "endo",
-    "reprod",
-    "develop",
-    "neuro_single",
-    "neuro_repeat",
-    "sys_single",
-    "sys_repeat",
-    "skin_sens",
-    "skin_irr",
-    "eye",
-    "aq_acute",
-    "aq_chron",
-    "persis",
-    "bioacc",
-    "expo"
-  ),
-  filter_results = FALSE,
-  filter_inc = list(
-    "stereo",
-    "chiral",
-    "isotopes",
-    "charged",
-    "multicomponent",
-    "radicals",
-    "salts",
-    "polymers",
-    "sgroups"
-  ),
-  element_inc = NULL,
-  element_exc = NULL,
-  mass_type = c("mono", "mw", "abu"),
-  min_mass = NULL,
-  max_mass = NULL,
-  debug = F,
-  ...
+	query,
+	searchType = c(
+		"exact",
+		"substruture",
+		"similar",
+		"mass",
+		"hazard",
+		"features"
+	),
+	similarity_type = c("tanimoto", "euclid", "tversky"),
+	min_similarity = NULL,
+	min_toxicity = c("VH", "H", "M", "L", "A"),
+	min_auth = c("auth", "screen", "qsar"),
+	hazard_name = c(
+		"acute_oral",
+		"acute_inhal",
+		"acute_dermal",
+		"cancer",
+		"geno",
+		"endo",
+		"reprod",
+		"develop",
+		"neuro_single",
+		"neuro_repeat",
+		"sys_single",
+		"sys_repeat",
+		"skin_sens",
+		"skin_irr",
+		"eye",
+		"aq_acute",
+		"aq_chron",
+		"persis",
+		"bioacc",
+		"expo"
+	),
+	filter_results = FALSE,
+	filter_inc = list(
+		"stereo",
+		"chiral",
+		"isotopes",
+		"charged",
+		"multicomponent",
+		"radicals",
+		"salts",
+		"polymers",
+		"sgroups"
+	),
+	element_inc = NULL,
+	element_exc = NULL,
+	mass_type = c("mono", "mw", "abu"),
+	min_mass = NULL,
+	max_mass = NULL,
+	debug = F,
+	...
 ) {
-  # searchType --------------------------------------------------------------
+	# searchType --------------------------------------------------------------
 
-  if (missing(searchType) | is.null(searchType) | length(searchType) > 1) {
-    cli_abort("Missing searchType!")
-  } else {
-    searchType <- case_when(
-      searchType == "exact" ~ "EXACT",
-      searchType == "substructure" ~ "SUBSTRUCTURE",
-      searchType == "similar" ~ "SIMILAR",
-      searchType == "toxprints" ~ "TOXPRINTS",
-      searchType == "hazard" ~ "HAZARD",
-      searchType == "mass" ~ "MASS",
-      searchType == "features" ~ "FEATURES"
-    )
-  }
+	if (missing(searchType) | is.null(searchType) | length(searchType) > 1) {
+		cli_abort("Invalid searchType!")
+	} else {
+		searchType <- case_when(
+			searchType == "exact" ~ "EXACT",
+			searchType == "substructure" ~ "SUBSTRUCTURE",
+			searchType == "similar" ~ "SIMILAR",
+			searchType == "toxprints" ~ "TOXPRINTS",
+			searchType == "hazard" ~ "HAZARD",
+			searchType == "mass" ~ "MASS",
+			searchType == "features" ~ "FEATURES"
+		)
+	}
 
-  # query -------------------------------------------------------------------
+	# query -------------------------------------------------------------------
 
-  if (missing(query) == TRUE) {
-    query <- NULL
-  }
+	if (missing(query) == TRUE) {
+		query <- NULL
+	}
 
-  query_string <- query
+	query_string <- query
 
-  if (searchType %in% c("HAZARD", "FEATURES")) {
-    mass_type <- "mono" # seems to be always needed?
-    query <- "\n  Ketcher  4112412132D 1   1.00000     0.00000     0\n\n  0  0  0     0  0            999 V2000\nM  END\n"
-  } else {
-    if (searchType %in% c("MASS")) {
-      query <- NULL
-    } else {
-      cli_alert_info(paste0("Grabbing MOL file for {query}"))
-    }
-    query <- ct_file(query = query)
-    mass_type <- "mono" # seems to be always needed?
-  }
+	if (searchType %in% c("HAZARD", "FEATURES")) {
+		mass_type <- "mono" # seems to be always needed?
+		query <- "\n  Ketcher  4112412132D 1   1.00000     0.00000     0\n\n  0  0  0     0  0            999 V2000\nM  END\n"
+	} else {
+		if (searchType %in% c("MASS")) {
+			query <- NULL
+		} else {
+				if(as.logical(Sys.getenv('run_verbose'))){
+					cli_alert_info(paste0("Grabbing MOL file for {query}"))
+				}
+		}
+		query <- ct_file(query = query)
+		#mass_type <- "mono" # seems to be always needed?
+		mass_type <- NULL
+	}
 
-  # records -----------------------------------------------------------------
+	# records -----------------------------------------------------------------
 
-  records <- 51L
+	records <- 51L
 
-  # similarity --------------------------------------------------------------
+	# similarity --------------------------------------------------------------
 
-  if (missing(similarity_type)) {
-    similarity_type <- "tanimoto"
-  }
+	if (missing(similarity_type)) {
+		similarity_type <- "tanimoto"
+	}
 
-  similarity_type <-
-    case_when(
-      similarity_type == "tanimoto" ~ "tanimoto",
-      similarity_type == "euclid" ~ "euclid-sub",
-      similarity_type == "tversky" ~ "tversky",
-    )
+	similarity_type <-
+		case_when(
+			similarity_type == "tanimoto" ~ "tanimoto",
+			similarity_type == "euclid" ~ "euclid-sub",
+			similarity_type == "tversky" ~ "tversky",
+		)
 
-  min_sim <- if (is.null(min_similarity)) {
-    0.85
-  } else {
-    as.numeric(min_similarity)
-  }
+	min_sim <- if (is.null(min_similarity)) {
+		0.85
+	} else {
+		min_sim <- as.numeric(min_similarity)
+		if(min_sim < 0 | min_sim > 1) {
+			cli_abort("Minimum similarity must be between 0 and 1!")
+		} else {
+			min_sim
+		}
+	}
 
-  # mass --------------------------------------------------------------------
+	# mass --------------------------------------------------------------------
 
-  if (searchType == "mass" & length(mass_type) > 1) {
-    cli_abort("Missing mass search type!")
-  } else {
-    mass_type <- case_when(
-      mass_type == "mono" ~ "monoisotopic-mass",
-      mass_type == "mw" ~ "moleculuar-weight",
-      mass_type == "abu" ~ "most-abundant-mass",
-    )
-  }
+	if (searchType == "mass" & length(mass_type) > 1) {
+		cli_abort("Missing mass search type!")
+	} else {
+		mass_type <- case_when(
+			mass_type == "mono" ~ "monoisotopic-mass",
+			mass_type == "mw" ~ "moleculuar-weight",
+			mass_type == "abu" ~ "most-abundant-mass",
+			.default = NULL
+		)
+	}
 
-  # toxicity ----------------------------------------------------------------
+	# toxicity ----------------------------------------------------------------
 
-  min_toxicity <-
-    if (missing(min_toxicity) | is.null(min_toxicity)) {
-      NULL
-    } else {
-      match.arg(min_toxicity, c("VH", "H", "M", "L", "A"))
-    }
+	min_toxicity <-
+		if (missing(min_toxicity) | is.null(min_toxicity)) {
+			NULL
+		} else {
+			match.arg(min_toxicity, c("VH", "H", "M", "L", "A"))
+		}
 
-  # auth --------------------------------------------------------------------
+	# auth --------------------------------------------------------------------
 
-  if (length(min_auth) > 1) {
-    min_auth <- NULL
-  } else {
-    min_auth <- case_when(
-      min_auth == "auth" ~ "Authoritative",
-      min_auth == "screen" ~ "Screening",
-      min_auth == "qsar" ~ "QSAR"
-    )
-  }
+	if (length(min_auth) > 1) {
+		min_auth <- NULL
+	} else {
+		min_auth <- case_when(
+			min_auth == "auth" ~ "Authoritative",
+			min_auth == "screen" ~ "Screening",
+			min_auth == "qsar" ~ "QSAR"
+		)
+	}
 
-  # hazard endpoint ---------------------------------------------------------
+	# hazard endpoint ---------------------------------------------------------
 
-  if (missing(hazard_name)) {
-    hazard_name <- NULL
-  }
+	if (missing(hazard_name)) {
+		hazard_name <- NULL
+	}
 
-  hazard_name <-
-    case_when(
-      hazard_name == "acute_oral" ~ "Acute Mammalian Toxicity Oral",
-      hazard_name == "acute_inhal" ~ "Acute Mammalian Toxicity Inhalation",
-      hazard_name == "acute_dermal" ~ "Acute Mammalian Toxicity Dermal",
-      hazard_name == "cancer" ~ "Carcinogenicity",
-      hazard_name == "geno" ~ "Genotoxicity Mutagenicity",
-      hazard_name == "endo" ~ "Endocrine Disruption",
-      hazard_name == "reprod" ~ "Reproductive",
-      hazard_name == "develop" ~ "Developmental",
-      hazard_name == "neuro_single" ~ "Neurotoxicity	",
-      hazard_name == "neuro_repeat" ~ "Neurotoxicity Repeat Exposure",
-      hazard_name == "sys_single" ~ "Systemic Toxicity Single Exposure",
-      hazard_name == "sys_repeat" ~ "Systemic Toxicity Repeat Exposure",
-      hazard_name == "skin_sens" ~ "Skin Sensitization",
-      hazard_name == "skin_irr" ~ "Skin Irritation",
-      hazard_name == "eye" ~ "Eye Irritation",
-      hazard_name == "aq_acute" ~ "Acute Aquatic Toxicity",
-      hazard_name == "aq_chron" ~ "Chronic Aquatic Toxicity",
-      hazard_name == "persis" ~ "Persistence",
-      hazard_name == "bioacc" ~ "Bioaccumulation",
-      hazard_name == "expo" ~ "Exposure"
-    )
+	hazard_name <-
+		case_when(
+			hazard_name == "acute_oral" ~ "Acute Mammalian Toxicity Oral",
+			hazard_name == "acute_inhal" ~ "Acute Mammalian Toxicity Inhalation",
+			hazard_name == "acute_dermal" ~ "Acute Mammalian Toxicity Dermal",
+			hazard_name == "cancer" ~ "Carcinogenicity",
+			hazard_name == "geno" ~ "Genotoxicity Mutagenicity",
+			hazard_name == "endo" ~ "Endocrine Disruption",
+			hazard_name == "reprod" ~ "Reproductive",
+			hazard_name == "develop" ~ "Developmental",
+			hazard_name == "neuro_single" ~ "Neurotoxicity	",
+			hazard_name == "neuro_repeat" ~ "Neurotoxicity Repeat Exposure",
+			hazard_name == "sys_single" ~ "Systemic Toxicity Single Exposure",
+			hazard_name == "sys_repeat" ~ "Systemic Toxicity Repeat Exposure",
+			hazard_name == "skin_sens" ~ "Skin Sensitization",
+			hazard_name == "skin_irr" ~ "Skin Irritation",
+			hazard_name == "eye" ~ "Eye Irritation",
+			hazard_name == "aq_acute" ~ "Acute Aquatic Toxicity",
+			hazard_name == "aq_chron" ~ "Chronic Aquatic Toxicity",
+			hazard_name == "persis" ~ "Persistence",
+			hazard_name == "bioacc" ~ "Bioaccumulation",
+			hazard_name == "expo" ~ "Exposure"
+		)
 
-  # features -----------------------------------------------------------------
+	# features -----------------------------------------------------------------
 
-  if (searchType == 'FEATURES' & filter_results == FALSE) {
-    cli_alert_warning('WARNING: Missing feature filters!')
-    filt_list <- list(NULL)
-  }
+	if (searchType == 'FEATURES' & filter_results == FALSE) {
+		cli_alert_warning('WARNING: Missing feature filters!')
+		filt_list <- list(NULL)
+	}
 
-  if (filter_results == FALSE & searchType != 'FEATURES') {
-    # cli_alert('No filter')
-    filt_list <- list(NULL)
-  } else {
-    if (searchType == 'FEATURES' & filter_results == TRUE) {
-      # cli_alert('Filtering')
-      filt_list <- list(
-        "charged",
-        "chiral",
-        "isotopes",
-        "multicomponent",
-        "polymers",
-        "radicals",
-        "salts",
-        "sgroups",
-        "stereo"
-      )
+	if (filter_results == FALSE & searchType != 'FEATURES') {
+		# cli_alert('No filter')
+		filt_list <- list(NULL)
+	} else {
+		if (searchType == 'FEATURES' & filter_results == TRUE) {
+			# cli_alert('Filtering')
+			filt_list <- list(
+				"charged",
+				"chiral",
+				"isotopes",
+				"multicomponent",
+				"polymers",
+				"radicals",
+				"salts",
+				"sgroups",
+				"stereo"
+			)
 
-      filt_list <- map(filter_inc, ~ is.element(., filt_list)) %>%
-        set_names(., ~ paste0("filter-", filter_inc))
-    }
-  }
+			filt_list <- map(filter_inc, ~ is.element(., filt_list)) %>%
+				set_names(., ~ paste0("filter-", filter_inc))
+		}
+	}
 
-  # elements ----------------------------------------------------------------
-  ## include ----------------------------------------------------------------
+	# elements ----------------------------------------------------------------
+	## include ----------------------------------------------------------------
 
-  if (!is.null(element_inc)) {
-    element_inc_orig <- element_inc
-    element_inc <- paste(element_inc, collapse = ",")
-  }
-  ## exclude ----------------------------------------------------------------
+	if (!is.null(element_inc)) {
+		element_inc_orig <- element_inc
+		element_inc <- paste(element_inc, collapse = ",")
+	}
+	## exclude ----------------------------------------------------------------
 
-  if (is.null(element_exc)) {
-    #print('No exlusion')
-    element_exc <- NULL
-  } else {
-    if (is.character(element_exc) & element_exc != 'ALL') {
-      # print(element_exc)
-      element_exc <- paste(element_exc, collapse = " ,")
-    } else {
-      if (element_exc == 'ALL') {
-				
+	if (is.null(element_exc)) {
+		#print('No exlusion')
+		element_exc <- NULL
+	} else {
+		if (is.character(element_exc) & element_exc != 'ALL') {
+			# print(element_exc)
+			element_exc <- paste(element_exc, collapse = " ,")
+		} else {
+			if (element_exc == 'ALL') {
 				# ! TODO Re-add the elements; seems to be broken here. Validate the upper limit for the elements
 
-        #print("Exlude all")
-        element_exc <- ComptoxR::pt$elements %>%
-          filter(., !c(Symbol %in% element_inc_orig)) %>%
-          filter(., as.numeric(Number) <= 103) %>%
-          select(Symbol) %>%
-          unique() %>%
-          unlist() %>%
-          sort() %>%
-          paste0(., collapse = ", ")
-        #print(element_exc)
-      }
-    }
-  }
+				#print("Exlude all")
+				element_exc <- ComptoxR::pt$elements %>%
+					filter(., !c(Symbol %in% element_inc_orig)) %>%
+					filter(., as.numeric(Number) <= 103) %>%
+					select(Symbol) %>%
+					unique() %>%
+					unlist() %>%
+					sort() %>%
+					paste0(., collapse = ", ")
+				#print(element_exc)
+			}
+		}
+	}
 
-  # payload -----------------------------------------------------------------
+	# payload -----------------------------------------------------------------
 
-  params <- list(
-    `limit` = 50,
-    `similarity-type` = similarity_type,
-    `min-similarity` = min_sim,
-    `min-toxicity` = min_toxicity,
-    `min-authority` = min_auth,
-    `hazard-name` = hazard_name,
-    `include-elements` = element_inc,
-    `exclude-elements` = element_exc,
-    `mass-type` = mass_type,
-    `min-mass` = min_mass,
-    `max-mass` = max_mass
-  ) %>%
-    append(filt_list) %>%
-    compact()
+	params <- list(
+		`limit` = 50,
+		`similarity-type` = similarity_type,
+		`min-similarity` = min_sim,
+		`min-toxicity` = min_toxicity,
+		`min-authority` = min_auth,
+		`hazard-name` = hazard_name,
+		`include-elements` = element_inc,
+		`exclude-elements` = element_exc,
+		`mass-type` = mass_type,
+		`min-mass` = min_mass,
+		`max-mass` = max_mass
+	) %>%
+		append(filt_list) %>%
+		compact()
 
-  payload <- list(
-    "inputType" = "MOL",
-    "searchType" = searchType,
-    "params" = params,
-    "query" = query
-  ) %>%
-    compact()
+	payload <- list(
+		"inputType" = "MOL",
+		"searchType" = searchType,
+		"params" = params,
+		"query" = query
+	) %>%
+		compact()
 
-  # payload alert -----------------------------------------------------------
+	# payload alert -----------------------------------------------------------
 
-  cli_text('\n')
-  cli_rule(left = "Payload options")
-  cli_dl(
-    c(
-      "Search type" = "{searchType}",
-      "Similarity type" = "{similarity_type}",
-      "Minimum simularity" = "{min_sim}",
-      "Minimum toxicity" = "{min_toxicity}"
-      # "Params" = "{params}"
-    )
-  )
-  cli_rule()
+	if (as.logical(Sys.getenv('run_verbose'))) {
+		cli_text('\n')
+		cli_rule(left = "Payload options")
+		# Display a formatted list of all search parameters by combining
+		# the searchType with the contents of the params list.
+		cli_dl(c(list("Search type" = searchType), params))
+		cli_rule()
+	}
 
-  # request -----------------------------------------------------------------
+	# request -----------------------------------------------------------------
 
-  response <- POST(
-    url = paste0(Sys.getenv("chemi_burl"), "/search"),
-    body = payload,
-    content_type("application/json"),
-    #accept("*/*"),
-    encode = "json",
-    progress()
-  )
+	response <- POST(
+		url = paste0(Sys.getenv("chemi_burl"), "/search"),
+		body = payload,
+		content_type("application/json"),
+		#accept("*/*"),
+		encode = "json",
+		progress()
+	)
 
-# debug -------------------------------------------------------------------
+	# debug -------------------------------------------------------------------
 
-if (as.logical(Sys.getenv('run_debug'))) {
-    data <- list()
-    data$payload <- payload
-    data$response <- response
-    data$content <- df
+	if (as.logical(Sys.getenv('run_debug'))) {
+		data <- list()
+		data$payload <- payload
+		data$response <- response
+		data$content <- df
 
-    return(data)
-}	
-	
-  if (response$status_code == 200) {
-    df <- content(response, "text", encoding = "UTF-8") %>%
-      fromJSON(simplifyVector = FALSE)
+		return(data)
+	}
 
-    trc <- df$totalRecordsCount
+	if (response$status_code == 200) {
+		df <- content(response, "text", encoding = "UTF-8") %>%
+			fromJSON(simplifyVector = FALSE)
 
-    df <- df %>%
-      pluck(., 'records') %>%
-      map(., as_tibble) %>%
-      list_rbind()
+		trc <- df$totalRecordsCount
 
-    if ('similarity' %in% colnames(df)) {
-      df <- df %>%
-        mutate(
-          relationship = case_when(
-            sid == query_string ~ 'parent',
-            .default = 'child'
-          )
-        )
-    } else {
-      df
-    }
+		df <- df %>%
+			pluck(., 'records') %>%
+			map(., as_tibble) %>%
+			list_rbind()
 
-    cli_alert_success('{trc} compounds found!')
+		if ('similarity' %in% colnames(df)) {
+			df <- df %>%
+				mutate(
+					relationship = case_when(
+						sid == query_string ~ 'parent',
+						.default = 'child'
+					)
+				)
+		} else {
+			df
+		}
+
+		cli_alert_success('{trc} compounds found!')
 		return(df)
-		
-  } else {
-    cli_alert_danger("\nBad request at search!")
-  }
-
+	} else {
+		cli_alert_danger("\nBad request at search!")
+	}
 }
-
