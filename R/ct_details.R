9136500dbe5bf4a554fe39197b58772b50a0270a
seanthimons <141747936+seanthimons@users.noreply.github.com>
2025-12-03 18:24:49 -0500

    feat: added two compounds to testing set, and rebuilt ct_details.

--- R/ct_details.R
+++ R/ct_details.R
@@ -3,90 +3,117 @@
 #'
 #' @param query A single DTXSID (in quotes) or a list to be queried
 #' @param projection A subset of date to be returned. By default returns a minimal set of common identifiers.
-#' @param ctx_api_keyChecks for API key in Sys env
 #'
 #' @return a data frame
 #' @export
 
 ct_details <- function(
-  query,
-  projection = c(
-    "all",
-    "standard",
-    "id",
-    "structure",
-    "nta",
-    'compact'
-  ),
-  ctx_api_key= NULL
+	query,
+	projection = c("all", "standard", "id", "structure", "nta", 'compact')
 ) {
-  if (is.null(ccte_api_key)) {
-    token <- ct_api_key()
-  }
+	if (missing(projection)) {
+		projection <- 'compact'
+	}
 
-  ctx_burl <- Sys.getenv("ctx_burl")
+	proj <- case_when(
+		projection == "all" ~ "chemicaldetailall",
+		projection == "standard" ~ "chemicaldetailstandard",
+		projection == "id" ~ "chemicalidentifier",
+		projection == "structure" ~ "chemicalstructure",
+		projection == "nta" ~ "ntatoolkit",
 
-  if (missing(projection)) {
-    projection <- 'compact'
-  }
+		projection == 'compact' ~ 'compact',
+		TRUE ~ NA_character_
+	)
 
-  proj <- case_when(
-    projection == "all" ~ "chemicaldetailall",
-    projection == "standard" ~ "chemicaldetailstandard",
-    projection == "id" ~ "chemicalidentifier",
-    projection == "structure" ~ "chemicalstructure",
-    projection == "nta" ~ "ntatoolkit",
+	query <- unique(as.vector(query))
 
-    projection == 'compact' ~ 'compact',
-    TRUE ~ NA_character_
-  )
+	if (length(query) == 0) {
+		cli::cli_abort("Query must be a character vector of DTXSIDs.")
+	}
 
-  cli_rule(left = 'Payload options')
-  cli_dl(
-    c('Projection' = '{proj}', 'Number of compounds' = '{length(query)}')
-  )
-  cli_rule()
+	init_query <- length(query)
+	batch_limit <- as.numeric(Sys.getenv("batch_limit"))
+	mult_count <- ceiling(length(query) / batch_limit)
+	mult_request <- mult_count > 1
 
-  surl <- "chemical/detail/search/by-dtxsid/"
+	if (length(query) > batch_limit) {
+		query_list <- split(
+			query,
+			rep(1:mult_count, each = batch_limit, length.out = length(query))
+		)
+	} else {
+		query_list <- list(query)
+	}
 
-  urls <- paste0(ctx_burl, surl, "?projection=", proj)
+	run_debug <- as.logical(Sys.getenv("run_debug", "FALSE"))
+	run_verbose <- as.logical(Sys.getenv("run_verbose", "FALSE"))
 
-  headers <- c(
-    `x-api-key` = token
-  )
+	if (run_verbose) {
+		cli_rule(left = 'Chemical Details payload options')
+		cli_dl(
+			c(
+				'Number of compounds' = '{length(query)}',
+				'Number of batches' = '{mult_count}',
+				'Projection' = '{proj}'
+			)
+		)
+		cli::cli_rule()
+		cli::cli_end()
+	}
 
-  if (length(query) > 200) {
-    sublists <- split(
-      query,
-      rep(
-        1:ceiling(length(query) / 200),
-        each = 200,
-        length.out = length(query)
-      )
-    )
-    sublists <- map(sublists, as.list)
-  } else {
-    sublists <- vector(mode = 'list', length = 1L)
-    sublists[[1]] <- query %>% as.list()
-  }
+	req_list <- map(
+		query_list,
+		function(query_part) {
+			request(Sys.getenv('ctx_burl')) %>%
+				req_method("POST") %>%
+				req_url_path_append("chemical/detail/search/by-dtxsid/") %>%
+				req_url_query("projection" = proj) %>% 
+				req_headers(
+					Accept = "application/json",
+					`x-api-key` = ct_api_key()
+				) %>%
+				req_body_json(
+					query_part,
+					auto_unbox = FALSE
+				)
+		}
+	)
 
-  df <- map(
-    sublists,
-    ~ {
-      response <- POST(
-        url = urls,
-        body = .x,
-        add_headers("x-api-key" = token),
-        encode = 'json',
-        progress()
-      )
-      df <- fromJSON(content(response, as = "text", encoding = "UTF-8"))
-    },
-    .progress = T
-  ) %>%
-    #TODO Probably needs to be validated for missing or NULL values
-    list_rbind()
+	if (as.logical(Sys.getenv('run_debug'))) {
+		return(req_list %>% pluck(., 1) %>% req_dry_run())
+	}
 
-  cli::cli_text()
-  return(df)
+	if (mult_request) {
+		resp_list <- req_list %>%
+			req_perform_sequential(., on_error = 'continue', progress = TRUE)
+	} else {
+		resp_list <- list(req_perform(req_list[[1]]))
+	}
+
+	body_list <- resp_list %>%
+		map(., function(r) {
+			if (resp_status(r) < 200 || resp_status(r) >= 300) {
+				cli::cli_abort(paste(
+					"API request failed with status",
+					resp_status(r)
+				))
+			}
+
+			body <- resp_body_json(r)
+
+			if (length(body) == 0) {
+				cli::cli_alert_warning("No results found for the given query.")
+				return(list())
+			}
+			return(body)
+		}) %>%
+		list_c()
+
+	final_cleaned <- body_list %>% 
+		map(., as_tibble) %>% 
+		list_rbind()
+	
+		return(final_cleaned)
+
 }
