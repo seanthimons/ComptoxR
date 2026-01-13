83c52fe1b48dcdcaef5b71d9b4bf36b72229bd14
seanthimons <thimons.sean@gmail.com>
2026-01-13 14:12:03 -0500

    feat: RQ codes now return tidy data or NULL, depreciated chemi_rq function.

--- R/chemi_safety_rqcodes.R
+++ R/chemi_safety_rqcodes.R
@@ -4,7 +4,7 @@
 #' `r lifecycle::badge("experimental")`
 #'
 #' @param query A list of DTXSIDs to search for
-#' @return Returns a tibble with results
+#' @return Returns a tibble with results (array of objects)
 #' @export
 #'
 #' @examples
@@ -12,13 +12,35 @@
 #' chemi_safety_rqcodes(query = "DTXSID7020182")
 #' }
 chemi_safety_rqcodes <- function(query) {
-  generic_chemi_request(
+  result <- generic_chemi_request(
     query = query,
     endpoint = "safety/rqcodes",
     server = "chemi_burl",
     auth = FALSE,
-    tidy = FALSE
+    tidy = FALSE,
+    wrap = FALSE
   )
+
+  # Additional post-processing can be added here
+
+  result <- result %>%
+    map(., ~ pluck(.x, 'rqCode')) %>%
+    compact()
+
+  if (length(result) > 0) {
+    result <- result %>%
+      map(., ~ as_tibble(.x)) %>%
+      list_rbind() %>%
+      tidyr::separate_wider_delim(rq, ' ', names = c('rq_lbs', 'rq_kgs')) %>%
+      dplyr::mutate(
+        rq_lbs = as.numeric(stringr::str_remove_all(rq_lbs, '\\(|\\)|\\,')),
+        rq_kgs = as.numeric(stringr::str_remove_all(rq_kgs, '\\(|\\)|\\,'))
+      )
+  }else{
+
+		result <- NULL
+
+	}
+
+  return(result)
 }
-
-
