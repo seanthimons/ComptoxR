f8e2726f96d971ae084039498e6a00ca40b0a5ee
seanthimons <141747936+seanthimons@users.noreply.github.com>
2026-01-21 15:59:31 -0500

    Cherry-pick changes from chemi_safety_rqcodes.R from 643a24c

--- R/chemi_safety_rqcodes.R
+++ R/chemi_safety_rqcodes.R
@@ -1,7 +1,7 @@
 #' Safety Rqcodes
 #'
 #' @description
-#' `r lifecycle::badge("experimental")`
+#' `r lifecycle::badge("stable")`
 #'
 #' @return Returns a tibble with results (array of objects)
 #' @export
@@ -19,7 +19,24 @@ chemi_safety_rqcodes <- function() {
 
   # Additional post-processing can be added here
 
+  result <- result %>%
+    map(., ~ pluck(.x, 'rqCode')) %>%
+    compact()
+
+  if (length(result) > 0) {
+    result <- result %>%
+      map(., ~ as_tibble(.x)) %>%
+      list_rbind() %>%
+      tidyr::separate_wider_delim(rq, ' ', names = c('rq_lbs', 'rq_kgs')) %>%
+      dplyr::mutate(
+        rq_lbs = as.numeric(stringr::str_remove_all(rq_lbs, '\\(|\\)|\\,')),
+        rq_kgs = as.numeric(stringr::str_remove_all(rq_kgs, '\\(|\\)|\\,'))
+      )
+  }else{
+
+		result <- NULL
+
+	}
+
   return(result)
 }
-
-
