9768806befdb41bd428f73d97563afb60e200b08
seanthimons <thimons.sean@gmail.com>
2026-08-07 14:21:47 -0400

    fix: repair chemi_amos Rd cross-reference and refresh generated contract tests

--- tests/testthat/test-chemi_predictor_qmrf.R
+++ tests/testthat/test-chemi_predictor_qmrf.R
@@ -30,7 +30,6 @@ test_that("chemi_predictor_qmrf passes request metadata to helper", {
   expect_equal(call[["endpoint"]], "predictor/qmrf/")
   expect_equal(call[["method"]], "GET")
   expect_equal(call[["batch_limit"]], 1)
-  expect_equal(call[["server"]], "chemi_burl")
   expect_equal(call[["auth"]], FALSE)
   expect_equal(call[["tidy"]], FALSE)
 })
