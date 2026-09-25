dccbe50758743ffd867ef0473527d5067d6e5856
Sean Thimons <thimons.sean@gmail.com>
2026-09-25 21:08:30 +0000

    fix: restore DESCRIPTION after wave roxygen checks

--- dev/specmill-rebuild/wave.R
+++ dev/specmill-rebuild/wave.R
@@ -271,7 +271,7 @@ roxygenise <- function() {
   stopifnot(system(sprintf("cd %s && Rscript -e 'roxygen2::roxygenise(roclets = c(\"rd\", \"namespace\"))' > /dev/null 2>&1", shQuote(work))) == 0L)
   docs <- c(file.path(work, 'NAMESPACE'), list.files(file.path(work, 'man'), pattern = '[.]Rd$', full.names = TRUE))
   hashes <- setNames(vapply(docs, digest::digest, '', file = TRUE, algo = 'sha256'), basename(docs))
-  stopifnot(system(sprintf('cd %s && git checkout -- man NAMESPACE && git clean -fdq man', shQuote(work))) == 0L)
+  stopifnot(system(sprintf('cd %s && git checkout -- man NAMESPACE DESCRIPTION && git clean -fdq man', shQuote(work))) == 0L)
   hashes
 }
 docs_before <- roxygenise()
